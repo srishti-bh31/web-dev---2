7be30df58104fbf65c421adc54fc4967783de409
srishti-bh31 <srishtibh31@gmail.com>
2026-01-21 15:42:51 +0530

    Add files via upload

---
+++ day 3/script.js
@@ -0,0 +1,35 @@
+//let sentence = " i am a comma separated sentence";
+//let word = sentence.split("a");
+//console.log(word)
+
+
+// q find out the occurence of "r' in the given string.
+// let str = "tu meri mai tera mai teri tu mera";
+/*  let word = str.split("r");
+console.log(word) */
+
+
+// synchronous function: we can move to another task afetr the previous task finishes its execution
+// function demo(){
+//     console.log("demo")
+// }
+// demo()
+
+
+// function sample(a,b){
+//   return a+b;
+// }
+//  console.log(sample(6,34))
+
+//arrow function
+//advantege: doesn'tneed return and function keyword and it will perform asynchronously.
+// const sample=(c,d)=> c+d;
+ // if we have multiple statement then we have to add return
+ //const test=(c,d)=>{
+ // return c+d;
+//}
+// console.log(test(4,6))
+
+
+let date =new Date();
+console.log(date)

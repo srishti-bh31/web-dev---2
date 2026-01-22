8c315d6d655ddab6b58bc4d94dd19c8a77917227
srishti-bh31 <srishtibh31@gmail.com>
2026-01-22 11:49:28 +0530

    Add files via upload

---
+++ day 4/script.js
@@ -0,0 +1,11 @@
+// let user= {
+//     fullname:"srishti bhardwaj",
+//     address: "delhi",
+//     mobileno:9625571727,
+//     favcolor:["pink","blue","green"]
+// }
+// console.log(user.address,user.fullname)
+// we can access the individual value by targeting key wiht variable name
+
+let date = new Date();
+console.log(date) 

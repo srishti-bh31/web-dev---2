3e09c1aba45516755d91b00a35ae6e6fd8d5e7d7
srishti-bh31 <srishtibh31@gmail.com>
2026-01-28 15:47:09 +0530

    Add files via upload

---
+++ day 6/script6.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById("hello")
+heading.style.color = "blue";
+heading.style.backgroundColor = "pink";
+
+const para = document.querySelector("#container p")
+para.style.color="red"

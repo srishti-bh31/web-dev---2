41d6dbd609502eb14d92b1eb8ea156581bb0c9d7
srishti-bh31 <srishtibh31@gmail.com>
2026-01-29 20:03:20 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,16 @@
+// task 1
+// const profile =document.getElementById("profile");
+// profile.style.background="pink";
+// profile.style.color="navy";
+// profile.style.textAlign ="center";
+// profile.style.border= "2px solid black";
+// profile.style.margin="20px";
+// profile.style.padding="20px";
+
+
+
+// task 2
+const highlight = document.getElementsByClassName('important');
+for (let i=0; i < highlight.length;i++){
+    highlight[i].style.color='red';
+}
